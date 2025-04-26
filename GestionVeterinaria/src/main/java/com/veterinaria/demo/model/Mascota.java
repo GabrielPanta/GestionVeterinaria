363bb9702caf69193655b177c510c181a285675e
gabrielpanta <gabrielpantajimenez@hotmail.com>
2025-04-26 12:47:51 -0500

    se importaron dependencias

--- GestionVeterinaria/src/main/java/com/veterinaria/demo/model/Mascota.java
+++ GestionVeterinaria/src/main/java/com/veterinaria/demo/model/Mascota.java
@@ -4,6 +4,14 @@
  */
 
 package com.veterinaria.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
 @Entity
 @Data
 public class Mascota {
