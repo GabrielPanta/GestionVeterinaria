f4ca51653b5e0aa8f73bd873388c8633f7e8b105
fertech727 <fercruzcor25@hotmail.com>
2025-04-26 13:16:20 -0500

    Se agrego repositorios

---
+++ GestionVeterinaria/src/main/java/com/veterinaria/demo/repository/DuenoRepository.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package com.veterinaria.demo.repository;
+
+/**
+ *
+ * @author fercr
+ */
+public interface DuenoRepository extends JpaRepository<Dueno, Long>{
+
+}
